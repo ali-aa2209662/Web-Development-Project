558d0c395de0661c0f46289cadbb0f54c7b1c550
3lyanJ <125390859+3lyanJ@users.noreply.github.com>
2026-05-02 16:52:55 +0300

    Post Repo

--- Social_Media Project/astra/public/Astra/JavaScript/auth.js
+++ Social_Media Project/astra/public/Astra/JavaScript/auth.js
@@ -1,11 +1,11 @@
-import { getData, saveData} from "./storage.js"
+import { getData, saveData } from "./storage.js"
 import { getUsers, createUser, setCurrentUser, getCurrentUser } from "./user.js";
 
 // console.log("loaded auth");
 // console.log(getCurrentUser())
 
-export function checkLogin(){// might make in phase 2
-  if (getCurrentUser()===null) logout(); //checks if user is logged in
+export function checkLogin() {// might make in phase 2
+  if (getCurrentUser() === null) logout(); //checks if user is logged in
 }
 
 export function login(username, password) {
@@ -20,39 +20,39 @@ export function login(username, password) {
   if (!user) {
     return false; // login failed
   }
-  // console.log("PPPPPPPPPPPPPPPHHHH")
+
   setCurrentUser(user.userid)
   window.location.href = "home.html";
 
 
-  }
+}
 
 export function signup(username, email, password) {
 
-    const users = getUsers();
+  const users = getUsers();
 
-    //Check if username already exists
-    const usernameTaken = users.some(u => u.username === username);
-    if (usernameTaken) {
+  //Check if username already exists
+  const usernameTaken = users.some(u => u.username === username);
+  if (usernameTaken) {
     return { success: false, message: "Username already exists" };
-    }
+  }
 
-    // 🔍 Check if email already exists
-    const emailTaken = users.some(u => u.email === email);
-    if (emailTaken) {
+  // 🔍 Check if email already exists
+  const emailTaken = users.some(u => u.email === email);
+  if (emailTaken) {
     return { success: false, message: "Email already in use" };
-    }
+  }
 
-    // adds new user to data
-    createUser(username, email, password);
+  // adds new user to data
+  createUser(username, email, password);
 
-    };
+};
 
-export function logout(){
+export function logout() {
   setCurrentUser(null);
   window.location.href = "login.html";
 }
 
 // add an event listener for the sumbit button for sign up
-// and make sure if all the information is correct to take 
+// and make sure if all the information is correct to take
 // the user to the login page after they submit
